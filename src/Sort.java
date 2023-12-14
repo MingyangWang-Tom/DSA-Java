64c6b51e96ac0f4e256005d916f6624bfea64338
MingyangWang-Tom <mingyang.wang97@gmail.com>
2023-12-14 10:46:15 -1000

    add insertion sort and quick sort

--- src/Sort.java
+++ src/Sort.java
@@ -8,11 +8,56 @@ public class Sort {
         }
 
         //bubbleSort(nums);
-        selectionSort(nums);
+        //selectionSort(nums);
+        //insertionSort(nums);
+        quickSort(nums, 0, nums.length-1);
 
         System.out.println("\nAfter sorting: ");
         for (int num : nums){
             System.out.print(num + " ");
+        }
+    }
+
+    public static void  quickSort(int[] nums, int low, int high){
+        if (low < high){
+            int pi = partition(nums, low, high);
+
+            quickSort(nums, low, pi - 1);
+            quickSort(nums, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] nums, int low, int high) {
+        int pi = nums[high];
+        int i = low - 1;
+
+        for (int j = low; j<= high; j++){
+            if (nums[j] < pi){
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+        int temp = nums[i+1];
+        nums[i + 1] = nums[high];
+        nums[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void insertionSort(int[] nums){
+        int size = nums.length;
+        for (int i = 1; i < size; i++){
+            int key = nums[i];
+            int j = i - 1;
+
+            while (j>= 0 && nums[j] > key){
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
         }
     }
 
@@ -49,6 +94,4 @@ public class Sort {
             }
         }
     }
-
-
 }
